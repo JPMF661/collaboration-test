b81836f5bfcb0dc29e949dd9b339f70c00e3f651
Bruno840 <bruno_fonte@sapo.pt>
2021-01-12 12:55:06 +0000

    +1

--- src/pt/jp/test/Main.java
+++ src/pt/jp/test/Main.java
@@ -2,13 +2,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		System.out.println("Hello João! I am António on the web.");
-		System.out.println("And this is António on the desktop...");
-		System.out.println("And a second add from my desktop");
 		int a = 13;
 		int b = 5;
 		System.out.println("The product of a and b is " + product(a, b));
-		System.out.println("test");
+		System.out.println("123");
 	}
 
 	private static int product(int a, int b) {
