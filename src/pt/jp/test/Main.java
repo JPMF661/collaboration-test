c8d43dc0578b18ec2e467b1691d5904fc334f41d
Jp <joao.paulo.martins.figueiredo@gmail.com>
2021-01-12 12:34:04 +0000

    added a product method

--- src/pt/jp/test/Main.java
+++ src/pt/jp/test/Main.java
@@ -2,6 +2,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		int a = 13;
+		int b = 5;
+		System.out.println("The product of a and b is " + product(a, b));
+	}
+
+	private static int product(int a, int b) {
+		return a*b;
 	}
 
 }
